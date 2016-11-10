f46b2511dbd397b18a3de06d8e5cfe6c9be3f0fa
Nuno Silva <nmsilva@ubiwhere.pt>
2016-11-10 17:37:57 +0000

    support do Django 1.9 installations

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ setup(
     name="drf-mongo-filters",
     packages=["drf_mongo_filters",],
     version="1.1",
-    requires=["mongoengine",
-              "djangorestframework"],
+    install_requires=["Django >= 1.7",
+                      "mongoengine >= 0.8, < 0.9",
+                      "djangorestframework >= 3.0, < 3.1"],
     # metadata for upload to PyPI
     author="Maxim Vasiliev",
     author_email="qwiglydee@gmail.com",
     description="Filtering support for Django Rest Framework Mongoengine.",
     keywords=["mongoengine", "django rest framework", "filtering"],
+    url="https://github.com/qwiglydee/drf-mongo-filters",
+    download_url="https://github.com/qwiglydee/drf-mongo-filters/releases",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
